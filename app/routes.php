e8ae3ba189947548bb606a7dba9bebf0a09e8647
Kevin Washington <kevw12188@gmail.com>
2014-09-26 14:23:54 -0400

    wrap up cache routes

--- app/routes.php
+++ app/routes.php
@@ -13,31 +13,60 @@
 
 Route::get('/', function()
 {
-	return View::make('hello');
+	//return View::make('hello');
+
+	return storage_path();
 });
 
 
 //show storing of data to the cache
 Route::get('/store', function(){
 
-return Cache::put('key', 'value', 60);
+return Cache::put('kevin', 'this is data', 60);
 
 });
 
 //show checking if data exists in the cache
 Route::get('/check', function(){
 
+	 if(Cache::has('kevin')){
+	 	return 'True';
+	 }
+	 else{
+	 	return 'Not in cache';
+	 }
 
 });
 
 //show retrieval of data from the cache
 Route::get('/retrieve', function(){
 
+	 if(Cache::has('kevin')){
+	 	
+	 	return Cache::get('kevin');
+	 }
+	 else{
+	 	
+	 	return 'not in cache';
+	 }
 
 });
 
 //show removal of data from the cache
 Route::get('/remove', function(){
 
+
+	 if(Cache::has('kevin')){
+	 	
+	 	return Cache::pull('kevin');
+	 }
+	 else{
+	 	
+	 	return 'not in cache';
+	 }
+
+
 });
+
+
 
